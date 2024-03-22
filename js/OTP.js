04c80681b379a0e55f8823315a7b4e2b52606c42
eabishenMansfield <162117080+eabishenMansfield@users.noreply.github.com>
2024-03-22 15:12:34 +0530

    Update OTP.js

--- js/OTP.js
+++ js/OTP.js
@@ -1,5 +1,12 @@
 const inputs = document.querySelectorAll(".otp-field input");
 
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(Flip, ScrollTrigger, TextPlugin, EasePack);
+});
+var tl = gsap.timeline();
+var tl2 = gsap.timeline();
+var tl3 = gsap.timeline();
+
 inputs.forEach((input, index) => {
   input.dataset.index = index;
   input.addEventListener("keyup", handleOtp);
@@ -86,13 +93,128 @@ function submit() {
   //   ***** for testing***** //
   //   ***** for testing***** //
 
-  var modal1 = new bootstrap.Modal(document.getElementById("successmodal"));
+  showSuccess();
 
-  modal1.show();
+  var end = Date.now() + 1 * 1000;
+  // go Buckeyes!
+  var colors = ["#ff69b4", "#ce009a", "#530196", "#00cc6d"];
+  (function frame() {
+    confetti({
+      particleCount: 4,
+      angle: 60,
+      spread: 55,
+      origin: {
+        x: 0,
+      },
+      colors: colors,
+    });
+    confetti({
+      particleCount: 4,
+      angle: 120,
+      spread: 55,
+      origin: {
+        x: 1,
+      },
+      colors: colors,
+    });
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
+  })();
 
   //   ***** for testing***** //
   //   ***** for testing***** //
   //   ***** for testing***** //
 
   // 👉 Call API below
+}
+
+function showSuccess() {
+  tl.to(".iconlock", {
+    duration: 0.5,
+    height: 0,
+    width: 0,
+  })
+    .to(".successicon", {
+      duration: 0.8,
+      height: 60,
+      width: 60,
+      ease: "elastic.out(1,0.5)",
+      border: "10px solid var(--light-color)",
+    })
+    .to(
+      ".OTPId",
+      {
+        duration: 1,
+        scale: 1.2,
+        ease: "elastic.out(1,0.9)",
+        opacity: 0,
+      },
+      "-=0.8"
+    )
+    .fromTo(
+      ".successID",
+      {
+        scale: 1.2,
+      },
+      {
+        // scale up and back to normal
+        duration: 0.8,
+        scale: 1,
+        ease: "elastic.out(1,0.5)",
+        opacity: 1,
+      },
+      "-=0.6"
+    );
+
+  tl2
+    .to(".supportheading", {
+      duration: 0.2,
+      scale: 1.1,
+      opacity: 0,
+      transformOrigin: "top",
+    })
+    .to(".supportheading", {
+      duration: 0,
+      position: "absolute",
+    });
+
+  tl3
+    .to(".otp-field input", {
+      duration: 1,
+      height: 0,
+      width: 0,
+      transformOrigin: "top",
+    })
+    .to(
+      ".otp-field",
+      {
+        duration: 0,
+        opacity: 0,
+        height: 0,
+        width: 0,
+        visibility: "hidden",
+        transformOrigin: "top",
+      },
+      "-=0.5"
+    );
+  // gsap.to(".landingInfo", {
+  //   duration: 0.5,
+  //   opacity: 0,
+  //   position: "absolute",
+  //   transform: "translateY(-05px)",
+  // });
+  gsap.fromTo(
+    ".landingHeading ",
+    {
+      scale: 0.8,
+      text: "OTP Verified",
+    },
+    {
+      duration: 0.8,
+      scale: 1,
+      ease: "elastic.out(1,1.1)",
+      marginBlockEnd: "1rem",
+    }
+  );
 }
